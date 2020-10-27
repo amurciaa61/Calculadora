dd6438f4d4535fe90aae65b79e06741c39a99ee6
Antonio Murcia <amurciaa61@gmail.com>
2020-10-27 01:19:18 +0100

    Agregar archivos de proyecto.

---
+++ Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string textoMostrar;
+        public MainWindow()
+        {
+            InitializeComponent();
+            textoMostrar = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string valor = (sender as Button).Tag.ToString();
+            textoMostrar += valor;
+            valorTextBlock.Text = textoMostrar;
+        }
+    }
+}
